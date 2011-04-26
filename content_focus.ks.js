c66cfa052d8906bb68310fef8f0bd26039b23ba1
basyura <basyura@gmail.com>
2011-04-26 22:33:18 +0900

    fixed wrong homepage url

--- content_focus.ks.js
+++ content_focus.ks.js
@@ -2,11 +2,11 @@ var PLUGIN_INFO =
 <KeySnailPlugin>
     <name>zou search</name>
     <name lang="ja">content focus</name>
-    <description>focus to content</description>
+    <description>focus to content with onload event</description>
     <version>0.0.1</version>
     <updateURL></updateURL>
     <iconURL></iconURL>
-    <author mail="basyura@gmail.com" homepage="http://github.com/basyura/content_focus">basyura</author>
+    <author mail="basyura@gmail.com" homepage="http://github.com/basyura/content-focus">basyura</author>
     <license>The MIT License</license>
     <license lang="ja">MIT ライセンス</license>
     <minVersion>1.8.8</minVersion>
