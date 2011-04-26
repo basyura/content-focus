887437c2d78d0e9e8f0bbcec363f82af2ff9ae55
basyura <basyura@gmail.com>
2011-04-26 21:30:53 +0900

    first commit

---
+++ content_focus.ks.js
@@ -0,0 +1,49 @@
+var PLUGIN_INFO =
+<KeySnailPlugin>
+    <name>zou search</name>
+    <name lang="ja">content focus</name>
+    <description>focus to content</description>
+    <version>0.0.1</version>
+    <updateURL></updateURL>
+    <iconURL></iconURL>
+    <author mail="basyura@gmail.com" homepage="http://github.com/basyura/content_focus">basyura</author>
+    <license>The MIT License</license>
+    <license lang="ja">MIT ライセンス</license>
+    <minVersion>1.8.8</minVersion>
+    <include>main</include>
+    <provides>
+    </provides>
+	<options>
+		<option>
+			<name>zou_search.user</name>
+			<type>string</type>
+			<description>hatena user id</description>
+			<description lang="ja">はてなのユーザID</description>
+		</option>
+	</options>
+    <detail><![CDATA[
+=== Usage ===
+=== 使い方 ===
+	
+履歴：
+0.0.1 initial
+    ]]></detail>
+</KeySnailPlugin>;
+
+gBrowser.addEventListener('load' , function(event) {
+    let doc = event.originalTarget;
+    if (!(doc instanceof HTMLDocument)) {
+      return;
+    }
+    alert(event.target.linkedBrowser.currentURI);
+    //alert(gBrowser.currentURI.spec);
+    setTimeout(function () {
+        let elem = document.commandDispatcher.focusedElement;
+        if (elem) {
+          elem.blur();
+        }
+        gBrowser.focus();
+        content.focus();
+      } , 100);
+  } , true);
+
